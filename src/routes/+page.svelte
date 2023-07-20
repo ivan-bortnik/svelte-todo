fbb7b906d7dad480b91ea835e5567babbc5a43e5
ibortnik <ivan.bortnik@etudian.univ-lr.fr>
2023-07-20 16:09:59 +0200

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,68 @@
+<script>
+
+    import TodoElement from "./TodoElement.svelte";
+
+    var todos = [
+        { text: "Test 1", isDone: false },
+        { text: "Test 2", isDone: true }
+    ]
+
+    var newTask = ""
+
+    function addTask() {
+        if (newTask != "")
+        {
+            todos.push({ text: newTask, isDone: false })
+            newTask = ""
+            
+            todos = todos
+        }
+    }
+
+    function removeTask(taskIdx) {
+        todos.splice(taskIdx, 1)
+
+        todos = todos
+    }
+
+    function bite() {
+        console.log("teub")
+    }
+
+</script>
+
+<main>
+
+    <h1>Tah la TODO List</h1>
+
+    {#each todos as todo, idx}
+
+        <TodoElement bind:text={todo.text} bind:done={todo.isDone} on:remove={() => removeTask(idx)}/>
+        
+    {/each}
+
+    <div id="new-todo">
+        <p>Ajouter une tâche</p>
+
+        <input type="text" bind:value={newTask}>
+
+        <button on:click={addTask}>OK</button>
+    </div>
+
+</main>
+
+<style>
+
+@import url('https://fonts.googleapis.com/css2?family=Nunito:wght@200;400;600;800&display=swap');
+
+main {
+    font-family: 'Nunito';
+    color: #222f3e;
+}
+
+#new-todo {
+    position: absolute;
+    bottom: 32px;
+}
+
+</style>
---
+++ src/routes/TodoElement.svelte
@@ -0,0 +1,68 @@
+<script>
+
+import { createEventDispatcher } from "svelte";
+const dispatch = createEventDispatcher()
+
+export var text
+export var done
+
+function removeTodo() {
+    dispatch('remove')
+}
+
+</script>
+
+<main>
+
+<div class={done ? "task-done" : ""}>
+
+    <input type="checkbox" bind:checked={done}>
+
+    <p>{text}</p>
+
+    <button on:click={removeTodo}>X</button>
+
+</div>
+
+</main>
+
+<style>
+    div {
+        position: relative;
+        border: 1px solid black;
+        border-radius: 8px;
+        display: flex;
+        overflow: hidden;
+        margin: 8px 0;
+    }
+
+    .task-done {
+        background-color: #1dd1a1;
+        color: white;
+    }
+
+    input {
+        margin: 0 16px;
+    }
+
+    button {
+        position: absolute;
+        right: 0;
+        height: 100%;
+        width: 64px;
+        border: none;
+        border-radius: 0;
+        background-color: #ee5253;
+        color: white;
+        font-weight: 800;
+        font-size: 22pt;
+        font-family: 'Nunito';
+        transition: .15s;
+    }
+
+    button:hover {
+        background-color: #ffffff;
+        color: #ee5253;
+        transition: .15s;
+    }
+</style>
